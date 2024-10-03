f0b30fcff69483ed65216ef6237564d452d9ee63
Palyo <prahladthoriya@aanibrothers.in>
2024-10-03 16:03:22 +0530

    update gradle

--- codespace/build.gradle.kts
+++ codespace/build.gradle.kts
@@ -61,3 +61,20 @@ dependencies {
     implementation(libs.browser)
     implementation(libs.gson)
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.coderspacedev"
+            artifactId = "codespace"
+            version = "1.0.8"
+
+            pom {
+                description.set("Codespace - A sample Android application")
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
