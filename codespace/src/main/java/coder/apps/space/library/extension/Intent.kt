9fd6bb10532a5c8472a4747da6df5809d4cd7c16
CodeSpace <techhuntdevelopers@gmail.com>
2025-03-28 11:27:41 +0530

    add intent new extension for getExtra

--- codespace/src/main/java/coder/apps/space/library/extension/Intent.kt
+++ codespace/src/main/java/coder/apps/space/library/extension/Intent.kt
@@ -4,6 +4,12 @@ import android.app.*
 import android.content.*
 import android.os.*
 
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import java.io.Serializable
+
 fun Activity.go(destination: Class<*>, extras: List<Pair<String, Any?>>? = null, finish: Boolean = false, finishAll: Boolean = false): Intent {
     return Intent(this, destination).apply {
         flags = if (finishAll) Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -37,5 +43,19 @@ fun Activity.goResult(destination: Class<*>, extras: List<Pair<String, Any?>>? =
                 is ArrayList<*> -> putExtra(key, value)
             }
         }
+    }
+}
+
+inline fun <reified T> Intent.getExtra(key: String, defaultValue: T): T {
+    return when (T::class) {
+        String::class -> getStringExtra(key) as? T ?: defaultValue
+        Int::class -> getIntExtra(key, defaultValue as? Int ?: 0) as T
+        Boolean::class -> getBooleanExtra(key, defaultValue as? Boolean ?: false) as T
+        Float::class -> getFloatExtra(key, defaultValue as? Float ?: 0f) as T
+        Double::class -> getDoubleExtra(key, defaultValue as? Double ?: 0.0) as T
+        Long::class -> getLongExtra(key, defaultValue as? Long ?: 0L) as T
+        Parcelable::class -> getParcelableExtra<Parcelable>(key) as? T ?: defaultValue
+        Serializable::class -> getSerializableExtra(key) as? T ?: defaultValue
+        else -> defaultValue
     }
 }
